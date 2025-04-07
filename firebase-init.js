e4c967c2ff5564afed017287f265509782044a64
DipeshNawani <dipeshnawani1610@gmail.com>
2025-04-07 10:17:11 +0530

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCtlAOBdM7bQ5Tmth4UeAw-QBoIU3Vl4BA",
+    authDomain: "healsmart-ai-836ef.firebaseapp.com",
+    projectId: "healsmart-ai-836ef",
+    storageBucket: "healsmart-ai-836ef.firebasestorage.app",
+    messagingSenderId: "640558731428",
+    appId: "1:640558731428:web:6c8835703f31d6635f9631",
+    measurementId: "G-QMDR2L0SVJ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig); 
